5e4a5961d1e0f66eed4fb037bd95d92ee91176e3
id-edm <ivandubochit@gmail.com>
2024-12-10 00:40:00 +0400

    feat: ready calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -309,8 +309,12 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const finalBalance = arr.reduce(
+    (balance, [income, expense]) => balance + (income - expense),
+    0
+  );
+  return finalBalance;
 }
 
 /**
