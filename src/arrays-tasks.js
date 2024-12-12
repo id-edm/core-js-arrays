c8445dba230d6da87c50135986264983250fdcf8
id-edm <ivandubochit@gmail.com>
2024-12-12 08:32:50 +0400

    feat: ready getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -451,8 +451,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const indicesOfOdd = numbers.reduce((acc, num, index) => {
+    if (num % 2 !== 0) acc.push(index);
+    return acc;
+  }, []);
+  return indicesOfOdd;
 }
 
 /**
