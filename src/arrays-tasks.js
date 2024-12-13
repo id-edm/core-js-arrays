b0e1dc2ad0390e5a8261cf4fbf8f875692983610
id-edm <ivandubochit@gmail.com>
2024-12-14 00:07:56 +0400

    feat: ready findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -522,8 +522,23 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  function recurse(index, currentLength, maxLength) {
+    if (index === nums.length) return maxLength;
+
+    let newLength = currentLength;
+    let newMaxLength = maxLength;
+
+    if (nums[index] > nums[index - 1]) {
+      newLength = currentLength + 1;
+    } else {
+      newLength = 1;
+    }
+    newMaxLength = Math.max(newMaxLength, newLength);
+    return recurse(index + 1, newLength, newMaxLength);
+  }
+
+  return recurse(0, 1, 0);
 }
 
 /**
